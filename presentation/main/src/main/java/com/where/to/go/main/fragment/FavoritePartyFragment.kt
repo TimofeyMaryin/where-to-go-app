f486232c83bc1352c57b28ea1ab33ea08b6af534
Ілона Мельник <66883153+TimofeyMaryin@users.noreply.github.com>
2024-12-21 20:46:49 +0500

    Ask question about FavoritePartyFragment.kt

--- presentation/main/src/main/java/com/where/to/go/main/fragment/FavoritePartyFragment.kt
+++ presentation/main/src/main/java/com/where/to/go/main/fragment/FavoritePartyFragment.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.where.to.go.component.AppText
+import com.where.to.go.component.TextSize
+import com.where.to.go.component.TextWeight
 import com.where.to.go.main.vms.RecommendedViewModel
 
 @Composable
@@ -20,7 +24,7 @@ fun FavoritePartyFragment(
     }
 
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Green))
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        AppText(text = "А в дизе что тут?", weight = TextWeight.MEDIUM, size = TextSize.BODY_LARGE)
+    }
 }
